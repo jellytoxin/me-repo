266ee539544d2b70a568700ab3af738b22d28c24
Alex <adaniels@stumail.fhtc.edu>
2016-08-26 09:32:13 -0500

    message

---
+++ TestingGit/src/ProgramJava.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class ProgramJava {
+	public Scanner in = new Scanner(System.in);
+	public static void main(String[] args) {
+		
+		in.close();
+		
+		
+		
+
+	}
+
+}
